f07abc2615c47e59285681ee252addc71a23adb9
Lincon Cardoso <59518299+lincon-cardoso@users.noreply.github.com>
2022-12-27 22:57:52 -0300

    Arrumar o itens salvo

--- js/main.js
+++ js/main.js
@@ -16,17 +16,19 @@ function handleSubmit(event) {
 
 function funcaoSeguraca(nome, quantidade) {
 
-   
+
     var Verifca = /^[^0-9].*[^0-9]$/;
-    if(Verifca.test(nome)){
+    if (Verifca.test(nome)) {
+        itemSalvoPagina(nome, quantidade);
         adicionaItem(nome, quantidade);
+        SalvaItemTela()
         limpaInputs()
-    }else {
-              
+    } else {
+
         limpaInputs()
     }
 
-  
+
 }
 
 function adicionaItem(nome, quantidade) {
@@ -35,7 +37,6 @@ function adicionaItem(nome, quantidade) {
     var li = document.createElement('li');
     var strong = document.createElement('strong');
     var lista = document.querySelector('ul');
-
     li.classList.add('item');
 
     // Define o conteúdo do elemento li como o valor do campo "nome e quantidade"
@@ -47,12 +48,88 @@ function adicionaItem(nome, quantidade) {
     li.insertBefore(strong, li.firstChild);
     //definindo li nome
     lista.appendChild(li);
-   
+
 
 }
 
-function limpaInputs(){
+function limpaInputs() {
     this.nome.value = '';
     this.quantidade.value = '';
 }
 
+
+
+
+// function itemSalvoPagina(nome,quantidade) {
+
+//     var itensArray = [];
+//     var item = {
+//         nome: nome,
+//         quantidade: quantidade
+//     }
+
+//     itensArray.push(item);
+
+
+//     let itensArrayString = JSON.stringify(itensArray)
+//     localStorage.setItem('itensArray',itensArrayString);
+//     let colorString = JSON.parse(localStorage.getItem("itensArray"));
+
+//     console.log(colorString)
+// }
+
+
+function itemSalvoPagina(nome, quantidade) {
+    // recupera o array armazenado no localStorage
+    let itensArray = JSON.parse(localStorage.getItem("itensArray"));
+    // se o array ainda não foi criado, cria um novo
+    if (!itensArray) {
+        itensArray = [];
+    }
+    // cria o novo item
+    var item = {
+        nome: nome,
+        quantidade: quantidade
+    }
+    // adiciona o item ao array
+    itensArray.push(item);
+    // armazena o array atualizado no localStorage
+    let itensArrayString = JSON.stringify(itensArray);
+    localStorage.setItem('itensArray', itensArrayString);
+}
+
+
+function SalvaItemTela() {
+    let itensArray = JSON.parse(localStorage.getItem("itensArray"));
+    // Percorre o array de itens
+    for (let i = 0; i < itensArray.length; i++) {
+        // Recupera o item atual
+        let item = itensArray[i];
+
+        var li = document.createElement('li');
+        var strong = document.createElement('strong');
+        var lista = document.querySelector('ul');
+        li.classList.add('item')
+        
+        strong.textContent = item.quantidade;
+        li.textContent = item.nome;
+
+        li.insertBefore(strong, li.firstChild);
+        //definindo li nome
+        lista.appendChild(li);
+    
+    
+    }
+    
+}
+
+
+
+// // Cria a div para o item
+// let div = document.createElement('div');
+
+// // Adiciona o nome e a quantidade do item à div
+// div.innerHTML = '<p>Nome: ' + item.nome + '</p><p>Quantidade: ' + item.quantidade + '</p>';
+
+// // Adiciona a div ao documento
+// document.body.appendChild(div);
